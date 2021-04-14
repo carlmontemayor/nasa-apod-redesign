185e92d62f665695d939a655a9fc2b3f6a8f77f1
Carl Montemayor <cmontemayor@nevada.unr.edu>
2021-04-13 21:18:08 -0700

    Updated tests

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -3,11 +3,6 @@ import { render, fireEvent } from '../testUtils'
 import { Home } from '../../pages/index'
 
 describe('Home page', () => {
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
-    expect(asFragment()).toMatchSnapshot()
-  })
-
   it('clicking button triggers alert', () => {
     const { getByText } = render(<Home />, {})
     window.alert = jest.fn()
